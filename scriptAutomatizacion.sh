b75679ea91fe817ae31af59981ef009dd679bb74
IvanSolaDelgado <soladelgadoivan@gmail.com>
2024-04-08 15:59:22 +0200

    Script actualizado con el git clone

--- scriptAutomatizacion.sh
+++ scriptAutomatizacion.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+cd
+echo "Clonando repositorio de git..."
+git clone https://github.com/IvanSolaDelgado/GPI.git
 
 # Crear directorio para los logs si no existe
 mkdir -p ~/logs
 
 # 1. Compilación con Maven
 echo "Iniciando compilación..."
-cd ~/sakaiGPI/kernel
+cd ~/GPI/kernel
 mvn clean install > ~/logs/compilacion.log 2>&1
-~/pmd-bin-7.0.0/bin/pmd check -d ~/sakaiGPI/kernel -f text -R rulesets/java/quickstart.xml > ~/logs/analisis.log 2>&1
+~/pmd-bin-7.0.0/bin/pmd check -d ~/GPI/kernel -f text -R rulesets/java/quickstart.xml > ~/logs/analisis.log 2>&1
 mvn test > ~/logs/tests.log 2>&1
 echo "Analisis y tests ejecutados"
